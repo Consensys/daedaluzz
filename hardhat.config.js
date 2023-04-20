632d85e02a3fad04460b579ac7f64a4a18d9d0e2
Valentin Wuestholz <valentin.wustholz@consensys.net>
2023-04-20 11:36:04 +0000

    Downgrade Solidity version.

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,7 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
     solidity: {
-        version: "0.8.19",
+        version: "0.8.18",
         settings: {
             optimizer: {
                 enabled: false,
