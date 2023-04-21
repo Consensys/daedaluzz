46af6504b86f799c287f4d4779487038204e4281
Chaofan Shou <shou@cs.ucsb.edu>
2023-04-20 19:21:24 -0700

    fix script crash

--- run-ityfuzz.sh
+++ run-ityfuzz.sh
@@ -2,13 +2,13 @@ cd /daedaluzz
 solc-select install 0.8.19
 solc-select use 0.8.19
 rm -rf ityfuzz-tmp/task-$1
-mkdir ityfuzz-tmp/task-$1
+mkdir -p ityfuzz-tmp/task-$1
 cd ityfuzz-tmp/task-$1
 rm -rf corpus
 mkdir corpus
 
-solc /daedaluzz/generated-mazes/maze-$3.sol --abi --bin --overwrite -o ityfuzz-tmp/ --optimize --optimize-runs 99999
+solc /daedaluzz/generated-mazes/maze-$3.sol --abi --bin --overwrite -o build/ --optimize --optimize-runs 99999
 
 TIME_LIMIT=$2
-timeout -n $TIME_LIMIT /bins/cli_print_logs -t "ityfuzz-tmp/*" | grep log
+timeout $TIME_LIMIT /bins/cli_print_logs -t "build/*" | grep log
 
