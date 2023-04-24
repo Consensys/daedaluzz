251ae77fd20516c6af12d3f7c0fbcf7136e01262
stanfordreject <chew@fuzz.land>
2023-04-24 20:08:39 +0000

    add pulling commands to run-all-fuzzer

--- run-all-fuzzers.sh
+++ run-all-fuzzers.sh
@@ -5,9 +5,12 @@ rm -rf foundry-tmp
 mkdir foundry-tmp
 rm -rf hybrid-echidna-tmp
 mkdir hybrid-echidna-tmp
+rm -rf ityfuzz-tmp
+mkdir ityfuzz-tmp
 docker pull ghcr.io/crytic/echidna/echidna:v2.1.1
 docker pull ghcr.io/foundry-rs/foundry@sha256:e3ba202249cccdffafc0d0e90c43baca8f03e4b0d7e273c0d33b8a5e3cea1eb7
 docker pull ghcr.io/crytic/echidna/echidna:v2.0.4
+docker pull fuzzland/ityfuzz:v0.0.1
 docker build --rm -t "hybrid-echidna:v0.0.2" -f Dockerfile.optik .
 # rm -rf optic-tmp
 # git clone https://github.com/crytic/optik.git optic-tmp
@@ -20,3 +23,4 @@ python3 run-campaigns.py --fuzzer-name harvey
 python3 run-campaigns.py --fuzzer-name echidna
 python3 run-campaigns.py --fuzzer-name foundry
 python3 run-campaigns.py --fuzzer-name hybrid-echidna
+python3 run-campaigns.py --fuzzer-name ityfuzz
